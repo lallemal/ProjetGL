16418b7b4bb1e21dbf2f0c473dce4913fe6abdf7
Louise Lallemand <lallemal@ensimag.grenoble-inp.fr>
2021-01-07 14:39:27 +0100

    test 2 decompile

--- src/main/java/fr/ensimag/deca/DecacMain.java
+++ src/main/java/fr/ensimag/deca/DecacMain.java
@@ -17,8 +17,7 @@ public class DecacMain {
     public static void main(String[] args) {
     	
         // example log4j message.
-        
-        //Test pas ouf pour voir git 
+       
     
         LOG.info("Decac compiler started");
         boolean error = false;

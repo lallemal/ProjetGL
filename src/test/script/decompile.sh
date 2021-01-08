f8edb4fc57fb9ecd4c16a748909c6bae9d4accb9
Louise Lallemand <lallemal@ensimag.grenoble-inp.fr>
2021-01-08 15:21:19 +0100

    test not yet ready but decompile.sh to automize it, wainting for lexer and parser

---
+++ src/test/script/decompile.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Auteur : gl40
+# Version initiale : 01/01/2021
+
+# Base pour un script de test de la lexicographie.
+# On teste un fichier valide et un fichier invalide.
+# Il est conseillé de garder ce fichier tel quel, et de créer de
+# nouveaux scripts (en s'inspirant si besoin de ceux fournis).
+
+
+# On se place dans le répertoire du projet (quel que soit le
+# répertoire d'où est lancé le script) :
+cd "$(dirname "$0")"/../../.. || exit 1
+
+PATH=./src/test/script/launchers:"$PATH"
+
+#(decac -p ./src/test/deca/decompile/PrintMultArg.deca) > PrintMultArg.test
+
+
+for i in src/test/deca/decompile/Print*.deca
+do
+  resultat=$(decac -p $i)
+  if [ "$(less $i.ok)" = "$resultat" ]; then
+        echo "Tout va bien"
+  else
+        echo "Résultat innatendu, le résultat:"
+        echo "$resultat"
+        echo "ce qui était attendu:"
+        echo "$(less $i.ok)"
+        exit 1
+  fi
+done

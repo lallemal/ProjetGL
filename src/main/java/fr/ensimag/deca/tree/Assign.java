de3428b6db19d6f6ca24b1c9ebc50887903d6c8e
Samuel Bacon <samuel.bacon@grenoble-inp.org>
2021-01-13 14:39:57 +0100

    on enleve ce coucou genant

--- src/main/java/fr/ensimag/deca/tree/Assign.java
+++ src/main/java/fr/ensimag/deca/tree/Assign.java
@@ -39,7 +39,6 @@ public class Assign extends AbstractBinaryExpr {
     
     @Override
     protected void codeGenInst(DecacCompiler compiler) {
-    	System.out.println("coucou");
         Identifier x = (Identifier) this.getLeftOperand();
         AbstractExpr e = this.getRightOperand();
         e.codeExp(compiler, 2);

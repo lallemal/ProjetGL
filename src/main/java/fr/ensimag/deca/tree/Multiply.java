6d85f4661236646123f78fc37560d21ac56f49e2
Samuel Bacon <samuel.bacon@grenoble-inp.org>
2021-01-23 16:51:27 +0100

    debut de la gestion de debordement arithmetique pour float

--- src/main/java/fr/ensimag/deca/tree/Multiply.java
+++ src/main/java/fr/ensimag/deca/tree/Multiply.java
@@ -3,6 +3,7 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.instructions.BOV;
 import fr.ensimag.ima.pseudocode.instructions.MUL;
 
 /**
@@ -17,6 +18,12 @@ public class Multiply extends AbstractOpArith {
     @Override
 	protected void mnemo(DecacCompiler compiler, DVal dval, int n) {
     	compiler.addInstruction(new MUL(dval, Register.getR(n)));
+    	if (this.getType().isFloat()) {
+    		if (!compiler.getCompilerOptions().isNoCheck()) {
+    			compiler.getLabelError().setErrorCalcFloat(true);
+    			compiler.addInstruction(new BOV(compiler.getLabelError().getLabelErrorcalcFloat()));
+    		}
+    	}
     }
 
     @Override
